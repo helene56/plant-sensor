327c7f21d07039a1af67d26ba4815c5571bd690f
helene <helene.maja@hotmail.dk>
2025-09-25 12:21:04 +0200

    simply log every 2 hours from start, simpler than having to calculate when to start logging.

--- src/logging_sensor.h
+++ src/logging_sensor.h
@@ -16,6 +16,4 @@ struct plant_log_data
 
 int64_t get_unix_timestamp_ms();
 void init_timer();
-int get_log_duration_min(int current_time_min, int log_time_min);
-int get_current_min_s(int64_t time);
 #endif /* LOGGING_SENSOR */
